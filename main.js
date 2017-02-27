b28c047aa6bf6eea1bab111d2e93987f4742b486
ninja25538 <ninja_william@hotmail.com>
2017-02-27 11:31:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+var connection=new WebSocket("ws://localhost:8787",'json');
+connection.onopen = function () {
+  connection.send('Hello, Server!!'); //send a message to server once connection is opened.
+};
+connection.onerror = function (error) {
+  console.log('Error Logged: ' + error); //log errors
+};
+connection.onmessage = function (e) {
+  console.log('Received From Server: ' + e.data); //log the received message
+};
