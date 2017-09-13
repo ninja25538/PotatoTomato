6541f6ff301bde0e92a6d71925a42831f8e29e67
ninja25538 <ninja25538@users.noreply.github.com>
2017-09-13 09:32:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ ctx.canvas.height = window.innerHeight - 100;
 ctx.fillStyle = "#1e90ff";
 ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-var scene = 0;
+var scene = 3;
 
 //All the stats about the  players
 var player1X = 200;
